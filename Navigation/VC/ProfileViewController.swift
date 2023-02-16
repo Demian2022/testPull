f0bccde68508c0857b342e83294671cb04d77bd2
Dmitry <s.workaut@yandex.ru>
2023-02-16 15:27:24 +0300

    create project

---
+++ Navigation/VC/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Mac ыыы on 28.01.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        view.backgroundColor = .white
+       
+    }
+
+
+}
---
+++ Navigation/profile/LogInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Mac ыыы on 16.02.2023.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+       
+    }
+    
+}
