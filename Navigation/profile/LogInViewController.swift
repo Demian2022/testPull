2871ca3111d933fd2617615e35e908fec17973e1
Dmitry <s.workaut@yandex.ru>
2023-02-16 15:43:49 +0300

    done

--- Navigation/profile/LogInViewController.swift
+++ Navigation/profile/LogInViewController.swift
@@ -8,10 +8,42 @@
 import UIKit
 
 class LogInViewController: UIViewController {
+    
+    private let emailField: UITextField = {
+            let field = UITextField()
+            field.autocapitalizationType = .none
+            field.autocorrectionType = .no
+            field.returnKeyType = .continue
+            field.layer.cornerRadius = 12
+            field.layer.borderWidth = 1
+            field.layer.borderColor = UIColor.lightGray.cgColor
+            field.placeholder = "Email Address..."
+            field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+            field.leftViewMode = .always
+            field.backgroundColor = .white
+            return field
+        }()
+        
+        private let passwordField: UITextField = {
+            let field = UITextField()
+            field.autocapitalizationType = .none
+            field.autocorrectionType = .no
+            field.returnKeyType = .done
+            field.layer.cornerRadius = 12
+            field.layer.borderWidth = 1
+            field.layer.borderColor = UIColor.lightGray.cgColor
+            field.placeholder = "Password..."
+            field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+            field.leftViewMode = .always
+            field.backgroundColor = .white
+            field.isSecureTextEntry = true
+            return field
+        }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
        
     }
     
